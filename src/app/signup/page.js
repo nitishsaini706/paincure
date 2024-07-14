a2351de14eb06aa25b33726780a0341086f6b7d8
nitish saini <nitish.saini@originscale.io>
2024-07-14 12:02:19 +0530

    fixing prod errors

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {  Button } from 'react-bootstrap';
+
 const Signup = () =>{
     return(
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
