399b3a003cfac812930a2d33f1a702409d77ea0e
nitish saini <nitish.saini@originscale.io>
2024-07-18 10:49:08 +0530

    home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ export default function Header({ bg = "white", color = "black" }) {
   };
 
   return (
-    <nav className={` lg:ml-20 mb:border-[1px] lg:border-0 mb:border-blue-950 lg:text-[20px] px-4 pt-4 pb-2 ${bg === 'white' ? ' border-b border-black bg-white' : 'bg-[#3D4966]'} ${color === 'black' ? 'text-black' : 'text-white'}`}>
+    <nav className={`sticky top-0 lg:ml-20 mb:border-[1px] lg:border-0 mb:border-blue-950 lg:text-[20px] px-4 pt-4 pb-2 ${bg === 'white' ? ' border-b border-black bg-white' : 'bg-[#3D4966]'} ${color === 'black' ? 'text-black' : 'text-white'}`}>
       <div className="lg:container lg:flex lg:justify-between">
         <div className="flex lg:items-center justify-between">
           
@@ -35,21 +35,21 @@ export default function Header({ bg = "white", color = "black" }) {
           </button>
         </div>
         </div>
-        <div className={`w-full block lg:flex lg:items-center lg:ml-[180px] lg:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
-          <div className="text-sm lg:flex-grow">
+        <div className={`w-full block lg:flex lg:items-center lg:ml-[80px] lg:w-auto ${isMenuOpen ? 'block' : 'hidden'} `}>
+          <div className="text-sm lg:flex-grow ">
             <Link href="/">
-              <p className={`block mt-4 lg:inline-block lg:mt-0 mx-2`}>Home</p>
+              <p className={`block  lg:inline-block lg:mt-0 lg:mr-[80px] mb:mt-5`}>Home</p>
             </Link>
             
             <div className="lg:relative lg:inline-block text-left">
-              <p onClick={toggleService} className={`block cursor-pointer mb:mt-2 lg:inline-block lg:mt-0 mx-2 ${color === 'black' ? 'text-black' : 'text-white'} bg-${bg === 'white' ? 'white' : 'gray-700'} hover:bg-opacity-75`}>
+              <p onClick={toggleService} className={`block cursor-pointer mb:mt-2 lg:inline-block lg:mt-0 lg:mr-[80px] ${color === 'black' ? 'text-black' : 'text-white'} bg-${bg === 'white' ? 'white' : 'gray-700'} hover:bg-opacity-75`}>
                 Services
               </p>
               {isServiceOpen && (
                 <div className="lg:origin-top-right lg:absolute mb:left-5 right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                   <div className="py-1">
                   <Link href="/services/neck-service">
-                      <p className="block px-4 py-2 text-sm text-gray-700">Neck Service</p>
+                      <p className="block px-4 py-2 text-sm text-gray-700">Neck Pain Service</p>
                       </Link>
                       <Link href="/">
                       <p className="block px-4 py-2 text-sm text-gray-700">Another action</p>
@@ -62,10 +62,10 @@ export default function Header({ bg = "white", color = "black" }) {
               )}
             </div>
             <Link href="/">
-              <p className={`block lg:mt-4 mb:mt-2 lg:inline-block  mx-2`}>About Us</p>
+              <p className={`block  mb:mt-2 lg:inline-block  lg:mr-[80px]`}>About Us</p>
               </Link>
               <Link href="/blogs">
-              <p className={`block lg:mt-4 mb:mt-2 lg:inline-block  mx-2`}>Blogs</p>
+              <p className={`block  mb:mt-2 lg:inline-block mb:mb-3`}>Blogs</p>
               </Link>
           </div>
           <div className="flex items-center mb:mt-4 mb:pb-2 lg:ml-[200px]">
